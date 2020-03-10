c25756d7930f44528b08e9e636e9a5c39b0b46e7
WilliamDag <32744303+WilliamDag@users.noreply.github.com>
2020-03-10 15:27:01 +0000

    Check in

---
+++ NotificationHubSample/NotificationHubSample.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: e00cd0c4370414b416c3ae6f4d19e2bb56b1baea */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1685
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 441471
+	.include	"typemap.jm.inc"
---
+++ NotificationHubSample/NotificationHubSample.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: e00cd0c4370414b416c3ae6f4d19e2bb56b1baea */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1685
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 441471
+	.include	"typemap.jm.inc"
